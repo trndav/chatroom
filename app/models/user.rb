d860b0c699b432ba32622e43be8c4c53f5cc79d2
trndav <farsiteguide@gmail.com>
2023-11-07 12:48:52 +0100

    Added user avatar on registration and edit user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,24 @@ class User < ApplicationRecord
   # After new record is committed to database added/brodcasted to "users" section in webpage realtime
   after_create_commit { broadcast_append_to "users" }
   has_many :messages
+  has_one_attached :avatar
+  after_commit :add_default_avatar, on: %i[create update]
+
+  def avatar_thumbnail 
+    avatar.variant(resize_to_limit: [140, 140]).processed
+  end
+
+  def chat_avatar 
+    avatar.variant(resize_to_limit: [50, 50]).processed
+  end
+
+  private
+  def add_default_avatar 
+    return if avatar.attached?
+      avatar.attach(
+        io: File.open(Rails.root.join("app", "assets", "images", "default_avatar.png")),
+        filename: "default_avatar.png",
+        content_type: "image/png"
+      )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  # get 'users/show'
   resources :rooms do
     resources :messages
   end
   
   root 'pages#home'
-  devise_for :users
-  devise_scope :user do
-    get "users", to: "devise/sessions#new"
-  end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  #devise_scope :user do
+    #get "users", to: "devise/sessions#new"
+  #end
 
   #resources :users
   # or
