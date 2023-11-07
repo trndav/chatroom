bf2528ce1422710d335528c9a2e23667cc6e3f91
trndav <farsiteguide@gmail.com>
2023-11-07 17:55:33 +0100

    Finished adding user avatar on chat

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_controller.default_url_options = { host: "localhost", port: 3000 }
+
   config.hosts << /[a-z0-9-]+\.ngrok-free\.app/
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
