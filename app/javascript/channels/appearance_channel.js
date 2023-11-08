5cadcd18daf95325cc9387b68832235810e71fe1
trndav <farsiteguide@gmail.com>
2023-11-08 20:31:50 +0100

    Finished and polished user activity status

--- app/javascript/channels/appearance_channel.js
+++ app/javascript/channels/appearance_channel.js
@@ -1,15 +1,75 @@
 import consumer from "channels/consumer"
 
+let resetFunc;
+let timer = 0;
+
 consumer.subscriptions.create("AppearanceChannel", {
+  initialized() {},
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("Connected");
+    resetFunc = () => this.resetTimer(this.uninstall);
+    this.install();
+    // turbo:load run after first visit and every turbo load
+    window.addEventListener("turbo:load", () => this.resetTimer());
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log("Disconnected");
+    this.uninstall();
   },
+  rejected() {
+    console.log("Rejected");
+    this.uninstall();
+  },  
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-  }
+  },
+  online() {
+    console.log("online");
+    this.perform("online");
+  },
+  away() {
+    console.log("away");
+    this.perform("away");
+  },
+  offline() {
+    console.log("offline");
+    this.perform("offline");
+  },
+  uninstall() {
+    const shouldRun = document.getElementById("appearance_channel");
+    if (!shouldRun) {
+      clearTimeout(timer);
+      this.perform("offline");
+    }
+  },
+  install() {
+    console.log("Install");
+    window.removeEventListener("load", resetFunc);
+    window.removeEventListener("DOMContentLoaded", resetFunc);
+    window.removeEventListener("click", resetFunc);
+    window.removeEventListener("keydown", resetFunc);
+
+    window.addEventListener("load", resetFunc);
+    window.addEventListener("DOMContentLoaded", resetFunc);
+    window.addEventListener("click", resetFunc);
+    window.addEventListener("keydown", resetFunc);
+    this.resetTimer();
+  },
+  resetTimer() {
+    this.uninstall();
+    const shouldRun = document.getElementById("appearance_channel");
+    if (!!shouldRun) { // check if we are on rightpage, double not?
+      this.online();
+      clearTimeout(timer);
+      const timeInSeconds = 120; // seconds to away
+      const miliseconds = 1000;
+      const timeInMiliseconds = timeInSeconds * miliseconds;
+
+      timer = setTimeout(this.away.bind(this), timeInMiliseconds);
+    }
+  },
 });
